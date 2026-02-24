9859534fe70eb56498d4b021e96b4c42e97683c0
zzq-ubuntu <2570365537@qq.com>
2026-02-24 16:41:42 +0800

    增加 bubbleSort 函数

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-
+void bubbleSort(int arr[], int size)
+{
+	int tmp = 0;
+	for(int i=0; i < size - 1; ++i)
+	{
+		for(int j=0; j < size - i - 1; ++j)
+		{
+			if(arr[j] < arr[j + 1]) 
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
 int main()
 {
 	std::cout << "hello git!\n";
